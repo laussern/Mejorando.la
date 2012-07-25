9f569109f0ada3260dd8fff385f6ce43c69e02d0
Leonidas Esteban <leonidasesteban@gmail.com>
2012-07-25 12:04:53 -0500

    formulario

--- static/js/formulario-viaje.js
+++ static/js/formulario-viaje.js
@@ -1,26 +1,33 @@
-jQuery(function ($) {
-	function enviando()
+$(document).ready(
+	function ()
 	{
-		$("#f-viaje #inscri").text("Inscribiendote...").fadeOut().fadeIn();
-	}
-
-	function recepcion(datos)
-	{
-		datos = JSON.parse(datos);
-
-		if(datos.error)
-		{
-			$("#f-viaje  #confirmacion").text("Verifica que todos los datos estén bien escritos").slideDown();
-		} else {
-			$("#f-viaje #inscri").text("¡Ya estás inscrito!").fadeOut().fadeIn();
-			$('#f-viaje')[0].reset()
-		}
-	}
-
-	var opciones = {
+		var opciones = {
 		beforeSubmit: enviando,
 		success: recepcion
-	};
+		};
 	
-	$('#f-viaje').ajaxForm(opciones); 
-});
+		$('#f-viaje').ajaxForm(opciones); 
+	}
+	
+);
+function enviando()
+{
+	$("#f-viaje #inscri").text("Inscribiendote...").fadeOut().fadeIn();
+}
+
+function recepcion(datos)
+{
+	datos = $.trim(datos);
+	if(datos=="OK")
+	{
+		$("#f-viaje #inscri").text("¡Ya estás inscrito!").fadeOut().fadeIn();
+		$('#f-viaje')[0].reset()
+	}
+	else
+	{
+		$("#f-viaje  #confirmacion").text("Verifica que todos los datos estén bien escritos").slideDown();
+	}
+	
+	
+	
+}
