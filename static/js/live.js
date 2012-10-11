9aa833d329cf7f3f1fb42771b948cac84655c33a
Leonidas Esteban <leonidasesteban@gmail.com>
2012-10-11 16:20:11 -0500

    puerto 3000 para chat

--- static/js/live.js
+++ static/js/live.js
@@ -29,7 +29,7 @@
 
 
 	$(window).on('load', function(){
-		$('#col2').html('<div class="chat"><iframe src="http://chat.mejorando.la" width="100%" height="75%" frameborder="0"></iframe></div>');
+		$('#col2').html('<div class="chat"><iframe src="http://chat.mejorando.la:3000" width="100%" height="75%" frameborder="0"></iframe></div>');
 	});
 
 
