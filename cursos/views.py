0f7380157e079e59d1efc2eedc5411edbcd49d80
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2012-11-10 14:36:09 -0600

    get corregido

--- cursos/views.py
+++ cursos/views.py
@@ -125,7 +125,7 @@ def curso(req, curso_slug):
 def paypal_ipn(req):
 	vs = req.POST.copy()
 
-	logging.error('PAYPAL IPN STARTED: payment_status %s, item_number %s, payer_mail %s' % (vs.get('payment_status', vs.get('item_number'), vs.get('payer_mail') )))
+	logging.error('PAYPAL IPN STARTED: payment_status %s, item_number %s, payer_mail %s' % (vs.get('payment_status'), vs.get('item_number'), vs.get('payer_mail')) )
 
 	# si estamos hablando de un pago
 	if vsPOST.get('payment_status') == 'Completed':
