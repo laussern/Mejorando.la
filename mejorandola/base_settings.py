3fa1df914c243315a3eeae0e90e0a23771b50a14
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2012-12-14 03:06:53 -0600

    arreglos para ipad

--- mejorandola/base_settings.py
+++ mejorandola/base_settings.py
@@ -72,11 +72,6 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'),
