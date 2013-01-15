372bd62a174a0d62a885077b727cb79728df65e6
Adán Miguel Sánchez Albert <dual.3nigma@me.com>
2013-01-15 16:20:24 -0600

    html en mails de errores

--- mejorandola/base_settings.py
+++ mejorandola/base_settings.py
@@ -158,7 +158,8 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
         }
     },
     'loggers': {
