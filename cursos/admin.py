b9545d593a42e424c200dbc83801d43f80a3a102
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2013-01-16 14:38:05 -0600

    filtro por version en registro tambien

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,52 +2,57 @@ from django.contrib import admin
 from models import Curso, CursoDia, CursoDocente, CursoRegistro, CursoPago
 from django.conf import settings
 
-class CursoAdmin(admin.ModelAdmin):
-	class Media:
-		js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL,
-			'%sjs/admin.js' % settings.STATIC_URL
-		)
 
-		css = {
-			'all': ('css/admin.css', )
-		}
+class CursoAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL,
+            '%sjs/admin.js' % settings.STATIC_URL
+        )
+
+        css = {
+            'all': ('css/admin.css', )
+        }
+
 
 class CursoDiaAdmin(admin.ModelAdmin):
-	class Media:
-		js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL,
-			'%sjs/admin.js' % settings.STATIC_URL
-		)
+    class Media:
+        js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL,
+            '%sjs/admin.js' % settings.STATIC_URL
+        )
 
-		css = {
-			'all': ('css/admin.css', )
-		}
+        css = {
+            'all': ('css/admin.css', )
+        }
+
 
 class CursoDocenteAdmin(admin.ModelAdmin):
-	class Media:
-		js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL,
-			'%sjs/admin.js' % settings.STATIC_URL
-		)
+    class Media:
+        js = ('%stiny_mce/tiny_mce.js' % settings.STATIC_URL,
+            '%sjs/admin.js' % settings.STATIC_URL
+        )
 
-		css = {
-			'all': ('css/admin.css', )
-		}
+        css = {
+            'all': ('css/admin.css', )
+        }
+
 
 class CursoRegistroAdmin(admin.ModelAdmin):
-	list_filter = ('pago__curso', 'pago__method', 'pago__charged', 'pago__pais')
-	search_fields = ('pago__nombre', 'pago__email')
+    list_filter = ('pago__curso', 'pago__method', 'pago__charged', 'pago__pais', 'pago__version')
+    search_fields = ('pago__nombre', 'pago__email')
 
-	def formfield_for_foreignkey(self, db_field, request, **kwargs):
-		if db_field.name == 'pago':
-			for i in kwargs:
-				print i
-			kwargs['queryset'] = CursoPago.objects.filter(charged=True).order_by('nombre')
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'pago':
+            for i in kwargs:
+                print i
+            kwargs['queryset'] = CursoPago.objects.filter(charged=True).order_by('nombre')
 
-		return super(CursoRegistroAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        return super(CursoRegistroAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 class CursoPagoAdmin(admin.ModelAdmin):
-	list_filter = ('curso', 'method', 'charged', 'pais', 'version')
-	search_fields = ('nombre', 'email')
-	list_display = ('nombre', 'email',)
+    list_filter = ('curso', 'method', 'charged', 'pais', 'version')
+    search_fields = ('nombre', 'email')
+    list_display = ('nombre', 'email',)
 
 admin.site.register(Curso, CursoAdmin)
 admin.site.register(CursoDia, CursoDiaAdmin)
