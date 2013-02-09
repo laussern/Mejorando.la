2149c98feade87ac926a7b51c4abcf65f86fccf2
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2013-02-08 20:55:58 -0600

    borrar

--- js/videos.js
+++
@@ -1,9 +0,0 @@
-require([
-    'jquery',
-    'vendor/jquery.lazyload'],
-    function ($, youtube, countdown) {
-        $("img").lazyload({
-            effect: 'fadeIn',
-            threshold: 200
-        });
-});
