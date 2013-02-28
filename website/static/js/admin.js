4bf7fcef8b3cf2b228613c0aa4955a0be4c4469d
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2013-02-28 12:41:21 -0600

    missing admin.js

---
+++ website/static/js/admin.js
@@ -0,0 +1,9 @@
+tinyMCE.init({
+	mode : "exact",
+	elements: "id_descripcion, id_temario, id_perfil, id_info_pago",
+	theme : "advanced",
+	theme_advanced_toolbar_location: "top",
+	theme_advanced_toolbar_align: "left",
+	forced_root_block: '',
+	height:300
+});
