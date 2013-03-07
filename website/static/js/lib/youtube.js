8cb7cd4d9883bf7b657f5df108b78208068b9486
Leonidas Esteban <leonidasesteban@gmail.com>
2013-03-07 11:18:35 -0500

    boton de youtube fix

--- website/static/js/lib/youtube.js
+++ website/static/js/lib/youtube.js
@@ -7,11 +7,16 @@ define([
                 var count = $.cookie('youtube_views'),
                     $youtube = $('.btn_youtube');
 
+                function formatNumber(num) {
+                    return num > 999 ? (num/1000).toFixed(1) + 'K' : num;
+                }
                 if (count) {
                     $youtube.find('.count').text(count);
                 } else {
                     $.getJSON('https://gdata.youtube.com/feeds/api/users/' + $youtube.data('id') + '?alt=json&callback=?', function (data) {
+
                         var count = formatNumber(data.entry['yt$statistics'].subscriberCount);
+
                         $.cookie('youtube_views', count);
                         $youtube.find('.count').text(count);
                     });
